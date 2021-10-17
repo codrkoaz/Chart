72104b6eb3650062ff51a04a3fcd8ac81c486a91
Joza Smith <artsy.joza@gmail.com>
2021-10-17 18:33:01 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,77 @@
+const salesChart = document.querySelector('#myChart').getContext('2d');
+
+let delayed;
+
+// Gradient fill
+let gradientFill = salesChart.createLinearGradient(0, 0, 4, 400);
+gradientFill.addColorStop(0, 'rgba(201,211,206,1)');
+gradientFill.addColorStop(1, 'rgba(157,0,43,.8)');
+
+//Labels years for Spirited Away
+const labels = [
+    '2010',
+    '2011',
+    '2012',
+    '2013',
+    '2014',
+    '2015',
+    '2016',
+    '2017',
+    '2018',
+    '2019',
+];
+
+//  Data sales for Spirited Away
+const data = {
+    labels,
+        datasets: [{
+            data: [10.5, 10.1, 10.8, 10.9, 10.3, 11.1, 11.3, 11, 11.8, 11.3],
+            label: 'Spirited Away Sales',
+            fill: true,
+            backgroundColor: gradientFill,
+            borderColor: '#437155',
+            color: '#9D002B',
+            pointBackgroundColor: '#DC8EB5',
+            tension: 0.3,
+        },
+    ],
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {
+        radius: 3.5,
+        hitRadius: 30,
+        hoverRadius: 10,
+        borderWidth: 1,
+        responsive: true,
+        animation: {
+            onComplete: () => {
+                delayed = true;
+            },
+            delay: (context) => {
+                let delay = 0;
+                if (context.type === 'data' && context.mode === 'default' && !delayed) {
+                    delay = context.dataIndex * 300 + context.datasetIndex * 100;
+                }
+                return delay;
+            },
+        },
+        scales: {
+            y: {
+                ticks: {
+                    callback: function (value) {
+                        return '$' + value + 'm';
+                    },
+                },
+            },
+        },
+    },
+    font: {
+        size: 20,
+    },
+};
+
+const myChart = new Chart(salesChart, config);
+
